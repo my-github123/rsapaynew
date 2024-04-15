76fde90217bccde5396e5289fde4de376965a86a
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-04-15 11:56:27 +0530

    YES

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 // router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
-router.get("/getUsers", userController.getAllUsers);
+// router.get("/getUsers", userController.getAllUsers);
 // router.put('/update', authMiddleware.verifyToken, userController.updateUser);
 // router.get('/getuser', authMiddleware.verifyToken, userController.getUserDetails);
 // router.delete('/delete/:id', userController.deleteUser);
