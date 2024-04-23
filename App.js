131a444b06eb4eb138444f7cd8ae4a3e68a4a675
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-04-23 17:40:06 +0530

    yes

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ app.use("/api/users", dashboardRoutes);
 app.use("/api/users", videoRoutes);
 app.use("/api/users", videoInteraction);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8443;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- src/model/User.js
+++ src/model/User.js
@@ -2,7 +2,7 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const User = sequelize.define("usersList", {
+const User = sequelize.define("user", {
   userId: {
     type: DataTypes.INTEGER,
     primaryKey: true,
