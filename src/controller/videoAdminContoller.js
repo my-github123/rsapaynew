7c7ab6280489b079ca66a14aae4ded1e97204ef7
yuvashree27 <yuvashree.s@gobumpr.com>
2024-05-02 09:56:30 +0530

    lang filter

--- src/controller/videoAdminContoller.js
+++ src/controller/videoAdminContoller.js
@@ -2,13 +2,14 @@ const AdminVideo = require("../model/VideoAdminModel");
 
 exports.createAdminPost = async (req, res) => {
   try {
-    const { adminUserId, url, name, description, date } = req.body;
+    const { adminUserId, url, name, description,language, date } = req.body;
     // const videoURL = req.file.path;
     const newPost = await AdminVideo.create({
       adminUserId,
       url,
       name,
       description,
+      language,
       date,
     });
 
@@ -19,11 +20,25 @@ exports.createAdminPost = async (req, res) => {
   }
 };
 
-exports.getVideosByAdminId = async (req, res) => {
-  const { adminUserId } = req.params;
+// exports.getVideosByAdminId = async (req, res) => {
+//   const { adminUserId } = req.params;
 
+//   try {
+//     const videos = await AdminVideo.findAll({ where: { adminUserId } });
+//     res.json(videos);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// };
+
+exports.getVideosByAdminId = async (req, res) => {
   try {
-    const videos = await AdminVideo.findAll({ where: { adminUserId } });
+    const videos = await AdminVideo.findAll({
+      where: { isEnabled: 1 },
+      group: ['name', 'url']
+    });
+
     res.json(videos);
   } catch (error) {
     console.error(error);
@@ -31,4 +46,30 @@ exports.getVideosByAdminId = async (req, res) => {
   }
 };
 
+exports.disableVideo = async (req, res) => {
+  const { videoIds, isEnabled } = req.body;
 
+  try {
+    // Update isEnabled to 0 for the selected videos
+    for (const videoId of videoIds) {
+      const result = await AdminVideo.update(
+        { isEnabled: isEnabled },
+        { where: { id: videoId } }
+      );
+
+      if (result[0] > 0) {
+        // Video was updated successfully
+        console.log(`Video ${videoId} updated: isEnabled set to ${isEnabled}`);
+      } else {
+        // No video found with the given ID
+        console.log(`No video found with ID ${videoId}`);
+      }
+    }
+
+    res.status(200).json({ success: true, message: 'Videos deleted successfully' });
+
+  } catch (error) {
+    console.error('Error deleting videos:', error);
+    res.status(500).json({ success: false, message: 'Failed to delete videos' });
+  }
+};
