ea10e933ba5324da86384fee03d72c0ff7fdeae8
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-07-09 11:09:54 +0530

    completed

--- src/routes/verifyVPARoutes.js
+++ src/routes/verifyVPARoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {verifyVPA}= require("../controller/verifyVPAController");
-const { transferPayment } = require('../controller/paymentController');
-const { getStatus } = require('../controller/statusController');
+// const { transferPayment } = require('../controller/paymentController');
+// const { getStatus } = require('../controller/statusController');
 
 router.post('/verifyVPA',verifyVPA);
-router.post('/transfer-payment', transferPayment);
-router.post('/get-status', getStatus);
+// router.post('/transfer-payment', transferPayment);
+// router.post('/get-status', getStatus);
 
 module.exports = router;
