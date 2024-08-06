e7a99e8b83ed22b25f3b6871e8793a14f505980c
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-08-06 11:58:58 +0530

    completed

--- src/controller/verifyVPAController.js
+++ src/controller/verifyVPAController.js
@@ -3,54 +3,42 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
-const fetch = require('node-fetch');
-
-
-
-
-
-
+const fetch = require("node-fetch");
 
 function encrypt(key, text) {
+  console.log(text, "TEXT IS THERE.................................");
+  const keyBuffer = Buffer.from(key, "hex");
+  const ivBuffer = Buffer.from([
+    0x8e, 0x12, 0x39, 0x9c, 0x07, 0x72, 0x6f, 0x5a, 0x8e, 0x12, 0x39, 0x9c,
+    0x07, 0x72, 0x6f, 0x5a,
+  ]);
 
-  console.log(text,"TEXT IS THERE.................................");
-  const keyBuffer = Buffer.from(key, 'hex');
-   const ivBuffer = Buffer.from([0x8E, 0x12, 0x39, 0x9C, 0x07, 0x72, 0x6F, 0x5A, 0x8E, 0x12, 0x39, 0x9C, 0x07, 0x72, 0x6F, 0x5A]);
-
-
-  
- 
-  
-
-  const cipher = crypto.createCipheriv('aes-128-cbc', keyBuffer, ivBuffer);
-  let encrypted = Buffer.concat([cipher.update(JSON.stringify(text), 'utf8'), cipher.final()]);
-  return Buffer.concat([ivBuffer, encrypted]).toString('base64');
+  const cipher = crypto.createCipheriv("aes-128-cbc", keyBuffer, ivBuffer);
+  let encrypted = Buffer.concat([
+    cipher.update(JSON.stringify(text), "utf8"),
+    cipher.final(),
+  ]);
+  return Buffer.concat([ivBuffer, encrypted]).toString("base64");
 }
-
-
-
-
-
-
-
 
 function decrypt(key, encrypted) {
   if (!key || !encrypted) {
-    throw new Error("Both key and encrypted arguments must be provided and be of type string.");
+    throw new Error(
+      "Both key and encrypted arguments must be provided and be of type string."
+    );
   }
-  
-  const keyBuffer = Buffer.from(key, 'hex');
-  const encryptedBuffer = Buffer.from(encrypted, 'base64');
+
+  const keyBuffer = Buffer.from(key, "hex");
+  const encryptedBuffer = Buffer.from(encrypted, "base64");
   const ivBuffer = encryptedBuffer.slice(0, 16);
   const ciphertextBuffer = encryptedBuffer.slice(16);
-  const decipher = crypto.createDecipheriv('aes-128-cbc', keyBuffer, ivBuffer);
-  
-  let decrypted = decipher.update(ciphertextBuffer, null, 'utf8');
-  decrypted += decipher.final('utf8');
-  
+  const decipher = crypto.createDecipheriv("aes-128-cbc", keyBuffer, ivBuffer);
+
+  let decrypted = decipher.update(ciphertextBuffer, null, "utf8");
+  decrypted += decipher.final("utf8");
+
   return decrypted;
 }
-
 
 const verifyVPA = async (req, res) => {
   console.log("Request Body is there........:", req.body);
@@ -64,12 +52,15 @@ const verifyVPA = async (req, res) => {
     });
   }
 
-  const keyAsHexString ='D8ABA26A5EA3126758F4F9A593BC573B';
+  const keyAsHexString = "D8ABA26A5EA3126758F4F9A593BC573B";
 
   // Convert hexadecimal string to Buffer
-  const keyBuffer = Buffer.from(keyAsHexString, 'hex');
+  const keyBuffer = Buffer.from(keyAsHexString, "hex");
   const concatenatedString = `${VerifyVPARequestBody.merchId}${VerifyVPARequestBody.merchChanId}${VerifyVPARequestBody.customerVpa}${VerifyVPARequestBody.corpCode}${VerifyVPARequestBody.channelId}`;
-  const md5Hash = crypto.createHash('md5').update(concatenatedString).digest('hex');
+  const md5Hash = crypto
+    .createHash("md5")
+    .update(concatenatedString)
+    .digest("hex");
 
   console.log(md5Hash);
 
@@ -79,10 +70,13 @@ const verifyVPA = async (req, res) => {
   const encryptedBody = encrypt(keyBuffer, VerifyVPARequestBody);
 
   // Define the API endpoint
-  const apiUrl = "https://sakshamuat.axisbank.co.in/gateway/api/txb/v1/acct-recon/verifyVPA";
+  const apiUrl =
+    "https://sakshamuat.axisbank.co.in/gateway/api/txb/v1/acct-recon/verifyVPA";
 
   // Path to your PFX certificate and passphrase
-  const pfxPath = path.resolve(__dirname, "../certificate/client.p12");
+   const pfxPath = path.resolve(__dirname, "../certificate/client.p12");
+
+
   const passphrase = "Year@2024"; // Replace with your actual passphrase
 
   try {
@@ -95,35 +89,40 @@ const verifyVPA = async (req, res) => {
       passphrase: passphrase,
     });
 
-    const apiBody = {
-      VerifyVPARequest: {
-        SubHeader,
-        VerifyVPARequestBodyEncrypted: encryptedBody,
-      }
-    };
+    // const apiBody = {
+    //   VerifyVPARequest: {
+    //     SubHeader,
+    //     VerifyVPARequestBodyEncrypted: encryptedBody,
+    //   },
+    // };
 
-    const body = JSON.stringify(apiBody);
+    // const body = JSON.stringify(apiBody);
 
-    console.log(body, "BODY IS THERE..");
+    // console.log(body, "BODY IS THERE..");
     console.log(encryptedBody, "ENCRYPTED BODY IS THERE...?");
+
+    const apiBody={"VerifyVPARequest":{"SubHeader":{"requestUUID":"ABC123","serviceRequestId":"OpenAPI","serviceRequestVersion":"1.0","channelId":"KIMOBILITY"},"VerifyVPARequestBodyEncrypted":"jhI5nAdyb1qOEjmcB3JvWiTF14NcPTUWmQUMdngOg9VLPHFMPI22fD23vKqJKgrhNoHJabVK3xbGcbVX/+7+dwRQCx7q/5ESWy6TxTJy0KPq2fF8sL19Inhm79mgOxPvNXgJrtZDg0NO9ZUwV1us0aaSIQqUb/q/sTTYOG3w5oPsrOxDmnOxl3pD5vlNNMvQ9+ENMfCc/tnHDS7aEYuGeHbI4vyTKi6XGLaT04Ngjy6YX4Ncxr8xggUuoC4vMYgieRWZnkiOL2BmqB9QgkDH6A=="}}
+
+    
 
     // Make the POST request to the external API with headers and host configuration
     const response = await fetch(apiUrl, {
-      method: 'POST',
-      body: body,
+      method: "POST",
+      body: apiBody,
       headers: {
         "Content-Type": "application/json",
         "X-IBM-Client-Id": "bf21e9bd4ad7ba83c4f04b31c2833302",
         "X-IBM-Client-Secret": "d58a28965d3640ffb470dcad05d12395",
       },
-      agent: httpsAgent // Pass the HTTPS agent
+      agent: httpsAgent, // Pass the HTTPS agent
     });
 
     const responseBody = await response.json();
     console.log(responseBody, "Response is there.....");
 
     // Decrypt the VerifyVPAResponseBodyEncrypted field in the response
-    const encryptedResponseBody = responseBody.VerifyVPAResponse.VerifyVPAResponseBodyEncrypted;
+    const encryptedResponseBody =
+      responseBody.VerifyVPAResponse.VerifyVPAResponseBodyEncrypted;
 
     const decryptedResponseBody = decrypt(keyBuffer, encryptedResponseBody);
 
@@ -132,7 +131,7 @@ const verifyVPA = async (req, res) => {
       VerifyVPAResponse: {
         SubHeader: responseBody.VerifyVPAResponse.SubHeader,
         VerifyVPAResponseBody: decryptedResponseBody,
-      }
+      },
     };
 
     // Send the response from the external API back to the client
@@ -141,10 +140,12 @@ const verifyVPA = async (req, res) => {
     console.error("Error Details:", {
       message: error.message,
       stack: error.stack,
-      response: error.response ? {
-        status: error.response.status,
-        data: error.response.data,
-      } : null,
+      response: error.response
+        ? {
+            status: error.response.status,
+            data: error.response.data,
+          }
+        : null,
     });
 
     // Send an error response if the API call fails
