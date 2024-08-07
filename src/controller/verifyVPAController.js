f1e8addb02d5d2374e87a7acd1013ab200868721
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-08-07 15:55:16 +0530

    YES

--- src/controller/verifyVPAController.js
+++ src/controller/verifyVPAController.js
@@ -108,11 +108,11 @@ const verifyVPA = async (req, res) => {
     console.log(body,"before API HIT.............");
     // Make the POST request to the external API with headers and host configuration
     const response = await axios.post(apiUrl, body, {
-      // headers: {
-      //   'Content-Type': 'application/json',
-      //   'X-IBM-Client-Id': 'bf21e9bd4ad7ba83c4f04b31c2833302',
-      //   'X-IBM-Client-Secret': 'd58a28965d3640ffb470dcad05d12395',
-      // },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-IBM-Client-Id': 'bf21e9bd4ad7ba83c4f04b31c2833302',
+        'X-IBM-Client-Secret': 'd58a28965d3640ffb470dcad05d12395',
+      },
       httpsAgent: httpsAgent, // Pass the HTTPS agent
     });
 
