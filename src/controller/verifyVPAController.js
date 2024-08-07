3b370466a74d0d079dff2fae1d8a967b4cedcc81
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-08-07 17:11:00 +0530

    completed

--- src/controller/verifyVPAController.js
+++ src/controller/verifyVPAController.js
@@ -120,6 +120,14 @@ const verifyVPA = async (req, res) => {
     
 
     const responseBody = await response.json();
+
+    console.log(response,"RESPONNSE I STHERE...");
+   console.log("----------------------------------------------------------------");
+   
+
+    console.log(responseBody,"response body..");
+    
+    
   
 
     // Decrypt the VerifyVPAResponseBodyEncrypted field in the response
@@ -146,7 +154,7 @@ const verifyVPA = async (req, res) => {
     console.log(pure,"CATCH INSIDE ...");
     // Send an error response if the API call fails
     return res.status(500).json({
-      message: "Error occurred from axis axis bank API",
+      message: "Error occurred from rahim API",
       error: error.message,
       status: error.response?.status || 500,
     });
