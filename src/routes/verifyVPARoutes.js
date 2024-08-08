d36043a5df21e8f2005ef869d0f54ac6989cbe0f
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-08-08 12:15:00 +0530

    complterd

--- src/routes/verifyVPARoutes.js
+++ src/routes/verifyVPARoutes.js
@@ -85,10 +85,13 @@ router.post('/verifyVPA', async (req, res) => {
       console.log(JSON.stringify(apiBody),"API body");
       
   
-      const apiUrl = "https://sakshamuat.axisbank.co.in/gateway/api/txb/v1/acct-recon/verifyVPA";
+     const apiUrl = "https://sakshamuat.axisbank.co.in/gateway/api/txb/v1/acct-recon/verifyVPA";
       const response = await axios.post(apiUrl, JSON.stringify(apiBody), {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          "X-IBM-Client-Id": "bf21e9bd4ad7ba83c4f04b31c2833302",
+          "X-IBM-Client-Secret": "d58a28965d3640ffb470dcad05d12395",
+
         },
         httpsAgent
       });
