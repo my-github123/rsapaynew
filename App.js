04e9d09eb1ad0c72412c11cb8d3aaf51e4eb84c8
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-09-11 13:34:06 +0530

    master

--- App.js
+++ App.js
@@ -123,7 +123,7 @@ app.get("/",(req,res)=>{
 // 192.168.2.8
 
 
-const PORT = process.env.PORT ||443;
+const PORT = process.env.PORT ||4100;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
