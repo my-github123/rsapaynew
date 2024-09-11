a87ac0fa7e2ca9dd85a1c31c58f6038ea672f43b
my-github123 <165751692+my-github123@users.noreply.github.com>
2024-09-11 18:49:13 +0530

    ii

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const serviceListController = require("../controller/serviceListController");
 
 router.post("/rsaCreateServiceList", serviceListController.createService);
-router.get("/rsaGetServiceList", serviceListController.getAllServices);
+router.get("/rsaGetServiceList1", serviceListController.getAllServices);
 router.delete("/rsaDeleteServiceAll", serviceListController.deleteAllServices);
 
 module.exports = router;
